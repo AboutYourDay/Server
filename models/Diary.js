240e5ffe4dc846ea54bd44a4db1a3d686eb342ab
Joylish <yeonzzu45@ajou.ac.kr>
2019-11-21 10:55:22 +0900

    create diary schema

---
+++ models/Diary.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const DiarySchema = new mongoose.Schema({
+    did: {type:String, required: true, unique: true},
+    uid: {type:String, required: true},
+    imgURL: String,
+    createdAt: Number,
+    editedAt: Number,
+    text: String,
+    emotion: String,
+});
+
+DiarySchema.statics.create = function(payload){
+    const diary = new this(payload);
+    return diary.save();
+}
+
+// DiarySchema.statics.findAll = function () {
+//     return this.find({});
+// };
+
+DiarySchema.statics.findByUid = function (uid) {
+    return this.find({ uid });
+};
+
+DiarySchema.statics.findByDid = function (did) {
+    return this.findOne({ did });
+};
+
+DiarySchema.statics.updateByDid = function (did, payload) {
+    return this.findOneAndUpdate({ did }, payload, { new: true });
+};
+
+// Delete by todoid
+DiarySchema.statics.deleteByDid = function (did) {
+    return this.remove({ did });
+};
+
+module.exports = mongoose.model('Diary', DiarySchema);
