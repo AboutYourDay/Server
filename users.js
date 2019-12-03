ae52ef6d08f5d071c616ea25f2e971cfa94ce06d
Joylish <yeonzzu45@ajou.ac.kr>
2019-12-03 14:28:15 +0900

    edit diaries and users routes

--- users.js
+++ users.js
@@ -4,9 +4,9 @@ const User = require("../models/User");
 //Find User by uid
 router.get('/:id', (req, res) => {
   User.findOne({_id: req.params.id})
-    .then(user => {
-      if (!user) return res.status(404).send({ "success": false, "err": "User not found" });
-      res.json({"success": true, "result": user});
+    .then(User => {
+      if (!User) return res.status(404).send({ "success": false, "err": "User not found" });
+      res.json({"success": true, "result": User});
     })
     .catch(err => res.status(500).send({"success": false, "err": err}));
 });
@@ -31,11 +31,15 @@ router.put('/:id', (req, res) => {
 
 // Delete by uid
 router.delete('/:id', (req, res) => {
-  User.remove({ _id: req.params.id }, movie => {
+  Movie.remove({ _id: req.params.id }, movie => {
     res.json({ "success": true });
     })
   .catch(err => res.json({ "success": false, "message": err }));
 
+  
+  // User.deleteByUid(req.params.uid)
+  //     .then(() => res.sendStatus(200))
+  //     .catch(err => res.status(500).send(err));
 
 });
 
