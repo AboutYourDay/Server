c483fafa8ad510984bd61c1621b807034101a3eb
Joylish <yeonzzu45@ajou.ac.kr>
2019-12-07 23:16:50 +0900

    edit the diary schema imageURL -> imageAttr and change the diaries post and put routes

--- models/Diary.js
+++ models/Diary.js
@@ -4,20 +4,27 @@ autoInc.initialize(mongoose.connection);
 
 const DiarySchema = new mongoose.Schema({
   uid: { type: String, required: true },
-  imageURL: { type: String, default: '' },
-  textAttr: { type: {
-    text: String,
-    alignHorizontal: String,
-    alignVertical: String,
-    fontSize: Number,
-    fontWeight: Number,
-    italic: Boolean,
-    underline: Boolean,
-    color: String
-  }, required: true },
+  imageAttr: {
+    width: Number,
+    height: Number,
+    imageURL: { type: String, default: "" }
+  },
+  textAttr: {
+    type: {
+      text: String,
+      alignHorizontal: String,
+      alignVertical: String,
+      fontSize: Number,
+      fontWeight: Number,
+      italic: Boolean,
+      underline: Boolean,
+      color: String
+    },
+    required: true
+  },
   emotion: { type: String, required: true },
   createdAt: { type: Number, required: true },
-  editedAt: { type: Number, required: true },
+  editedAt: { type: Number, required: true }
 });
 
 DiarySchema.plugin(autoInc.plugin, "Diary");
